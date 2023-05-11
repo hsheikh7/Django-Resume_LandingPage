777e8e826bef6fdb7be6a73aff4889b6c8b1f5f4
hsheikh7 <hassan.sheikh85@gmail.com>
2023-05-11 20:26:57 +0430

    Initial setup of website

--- website/urls.py
+++ website/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from website.views import *
 
+app_name = 'website'
+
 urlpatterns = [
 path('', index_view ),  
-    
+path('contact',contact_view,name='contact'),
+
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
+from website.forms import ContactForm
+from django.contrib import messages
 
 # Create your views here.
 def index_view(request):
     return render(request, 'index.html')
+
+def contact_view(request):
+    if request.method == 'POST':
+        
+        form = ContactForm(request.POST)
+        
+        if form.is_valid():
+            
+            form.save()
+            messages.add_message(request,messages.SUCCESS,'Your message submited successfully')
+        else:
+            messages.add_message(request,messages.ERROR,'Oops! Something goes wrong!')
+            
+    form = ContactForm()
+    return render(request,'index.html',{'form':form})
